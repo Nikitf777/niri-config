8656e2238fe6bb43d066e2d8db21254fbe8aad85
Nikita Samusev <nikitf777@gmail.com>
2026-02-15 00:25:25 +0300

    Install packages before everything else

--- apply-config.sh
+++ apply-config.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
+./install-packages.sh
 cd niri/scripts
 ./apply-all-custom-shaders.sh
 cd ../..
 ./apply-niri-config.sh
-./install-packages.sh
 ./apply-delta-shell-config.sh
 ./apply-systemd-config.sh
 ./apply-hypr-config.sh
